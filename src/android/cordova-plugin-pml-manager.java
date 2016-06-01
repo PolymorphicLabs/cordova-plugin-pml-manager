5c756a53f69a1dd1404ac55e350904e5e24a58ae
Trey German <treygerman@ti.com>
2016-06-01 13:03:07 -0500

    fixed plugin namespace issues

--- src/android/cordova-plugin-pml-manager.java
+++ src/android/cordova-plugin-pml-manager.java
@@ -1,4 +1,4 @@
-package cordova-plugin-pml-manager;
+package com.polymorphiclabs.cordova.plugin.pml;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class cordova-plugin-pml-manager extends CordovaPlugin {
+public class manager extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
