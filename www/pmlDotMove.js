29fd619924afe83097ffedb8f4cd83fc286972ef
Trey German <treygerman@ti.com>
2016-09-20 17:29:27 -0500

    Dot updates

--- www/pmlDotMove.js
+++ www/pmlDotMove.js
@@ -1,30 +1,43 @@
+/**
+ * Polymorphic Labs dot Move Module
+ * @module pmlDotMove
+ */
 "use strict";        
 
 var deviceId;
 var hwDefs;
 
-exports.setDeviceId = function(device){
-    deviceId = device;
-};
-
+/** Set the version of hwdefs used for this device. 
+ * @param {object} defs - Specific hardware defs for the version of the device being interacted with.
+ */
 exports.setHwDefs = function(defs){
     hwDefs = defs;
 };
 
-//Connection managment
+/**
+ * AHRS devices that are currently connected.
+ */
 exports.connections = [];
 
+/** Adds device to the connected device array.
+ * @param {object} device - Device object to add to connection array.
+ */
 exports.newConnection = function(device){
     //Add device to connection list and return handle
     exports.connections.push(device.id);
     return device;
 };
 
-exports.termConnection = function(device){
+/** Removes a device from the connected device array.
+ * @param {object} device - Device object to remove from connection array.
+ */
+exports.termConnection = function(device) {
     //Remove device from list 
     var index = exports.connections.indexOf(device.id);
     if(index != -1)
     	exports.connections.splice(index, 1);
+
+    //TODO: Deregister any callbacks
 };
 
 	 
@@ -151,151 +164,226 @@ exports.termConnection = function(device){
 		 readMoveConfig(handle, onRead);
 	 };
 	 
-	//************************************************************************
-	 //AHRS Service Functions
-	 //************************************************************************
-	 var ahrsCallback;
-	 var readAhrsConfig = function(callback){
-		 ble.read(deviceId, hwDefs.ahrs.service, hwDefs.ahrs.configuration,callback,function(error){console.log(error);});
-	 }
-	 exports.registerAhrsCallback = function(callback){
-		 ahrsCallback = callback;
-	 };
-	 exports.enableAhrsCallback = function(){
-		 ble.startNotification(deviceId, hwDefs.ahrs.service, hwDefs.ahrs.data, ahrsCallback, function(error){console.log(error);});
-	 };
-	 exports.disableAhrsCallback = function(){
-		 ble.stopNotification(deviceId, hwDefs.ahrs.service, hwDefs.ahrs.data, function(){console.log("AHRS Notifications Stopped");}, function(error){console.log(error);});
-	 };
-	 exports.setAhrsPeriod = function(period){
-		 //TODO: Add math to calculate period value
-		 var periodData = new Uint8Array(1);
-		 periodData[0] = period;
-		 ble.write(deviceId, hwDefs.ahrs.service, hwDefs.ahrs.period, periodData.buffer,
-		     function() { console.log("Configured AHRS period."); },function(error){console.log(error);});
-		 
-	 };
-	 exports.enableAhrs = function(){
-		 var configData = new Uint8Array(1);
-		 configData[0] = 1;	//Enable AHRS
-         ble.write(deviceId, hwDefs.ahrs.service, hwDefs.ahrs.configuration, configData.buffer, 
-                 function() { console.log("Enabled AHRS."); },function(error){console.log(error);});
-
-	 };
-	 exports.disableAhrs = function(){
-		 var configData = new Uint8Array(1);
-		 configData[0] = 0;	//Disable AHRS
-         ble.write(deviceId, hwDefs.ahrs.service, hwDefs.ahrs.configuration, configData.buffer, 
-                 function() { console.log("Disabled AHRS."); },function(error){console.log(error);});
-	 };
-	 
 	 
 	 //************************************************************************
 	 //Barometer Service Functions
 	 //************************************************************************
-	 var baroCallback;
-	 exports.registerBaroCallback = function(callback){
-		 baroCallback = callback;
+	 var baroCallbacks = [];
+
+     /**
+ * Registers a callback for when raw movement data is recieved.
+ * @param {string} handle - MAC address string of the device you're registering the callback for.
+ * @param {function} callback - Callback function to recieve data.
+ */
+	 exports.registerBaroCallback = function(handle, callback){
+    baroCallbacks.push({
+        handle,
+        callback
+    });
 	 };
-	 exports.enableBaroCallback = function(){
-		 ble.startNotification(deviceId, hwDefs.barometer.service, hwDefs.barometer.data, baroCallback, function(error){console.log(error);});
+
+/**
+ * Enables the raw movement data to be sent over bluetooth. The device must also be in the correct 
+ * operating mode in order to recieve non-zero data.  Note: calls to configuration functions should 
+ * be spaced by a few miliseconds by calling them with setTimeout.
+ * @param {string} handle - MAC address string of the device you're enabling the callback for.
+ */
+	 exports.enableBaroCallback = function(handle){
+        function findHandle(callbacks) {
+            return callbacks.handle === handle;
+        }
+
+		 ble.startNotification(handle, hwDefs.barometer.service, hwDefs.barometer.data, baroCallbacks.find(findHandle).callback, function(error){console.log(error);});
+
+         var configData = new Uint8Array(1);
+		 configData[0] = 1;
+		 ble.write(handle, hwDefs.barometer.service, hwDefs.barometer.configuration, configData.buffer,
+		     function() { console.log("Enabled barometer."); },function(error){console.log(error);});
 	 };
-	 exports.disableBaroCallback = function(){
-		 ble.stopNotification(deviceId, hwDefs.barometer.service, hwDefs.barometer.data, function(){console.log("Barometer Notifications Stopped");}, function(error){console.log(error);});
+
+
+/**
+ * Disables the raw movement data being sent over bluetooth.
+ * @param {string} handle - MAC address string of the device you're disabling the callback for.
+ */
+	 exports.disableBaroCallback = function(handle){
+		 ble.stopNotification(handle, hwDefs.barometer.service, hwDefs.barometer.data, function(){console.log("Barometer Notifications Stopped");}, function(error){console.log(error);});
+
+         var configData = new Uint8Array(1);
+		 configData[0] = 0;
+		 ble.write(handle, hwDefs.barometer.service, hwDefs.barometer.configuration, configData.buffer,
+		     function() { console.log("Disabled barometer."); },function(error){console.log(error);});
 	 };
-	 exports.setBaroPeriod = function(period){
+
+/**
+ * Sets the movement data period of the AHRS device. 
+ * @param {string} handle - MAC address string of the device you're changing the movement data period of.
+ * @param {value} mode - Integer value between 10 and 255.  Resolution is 10ms with a minimum of 100ms period.  For example, passing 10 yields 10 * 10ms = 100ms period.
+ */
+	 exports.setBaroPeriod = function(handle, period){
 		 //TODO: Add math to calculate period value
 		 var periodData = new Uint8Array(1);
 		 periodData[0] = period;
-		 ble.write(deviceId, hwDefs.barometer.service, hwDefs.barometer.period, periodData.buffer,
+		 ble.write(handle, hwDefs.barometer.service, hwDefs.barometer.period, periodData.buffer,
 		     function() { console.log("Configured barometer period."); },function(error){console.log(error);});
 		 
 	 };
-	 exports.enableBaro = function(){
-		 var configData = new Uint8Array(1);
-		 configData[0] = 1;
-		 ble.write(deviceId, hwDefs.barometer.service, hwDefs.barometer.configuration, configData.buffer,
-		     function() { console.log("Enabled barometer."); },function(error){console.log(error);});
-	 };
-	 exports.disableBaro = function(){
-		 var configData = new Uint8Array(1);
-		 configData[0] = 0;
-		 ble.write(deviceId, hwDefs.barometer.service, hwDefs.barometer.configuration, configData.buffer,
-		     function() { console.log("Disabled barometer."); },function(error){console.log(error);});
-	 };
+
+
+
+
 	 
 	 //************************************************************************
 	 //IR Thermometer Service Functions
 	 //************************************************************************
-	 var irThermCallback;
-	 exports.registerThermCallback = function(callback){
-		 irThermCallback = callback;
+	 var irThermCallbacks = [];
+     /**
+ * Registers a callback for when raw movement data is recieved.
+ * @param {string} handle - MAC address string of the device you're registering the callback for.
+ * @param {function} callback - Callback function to recieve data.
+ */
+	 exports.registerThermCallback = function(handle, callback){
+		 irThermCallbacks.push({
+            handle,
+            callback
+        });
 	 };
-	 exports.enableThermCallback = function(){
-		 ble.startNotification(deviceId, hwDefs.irTherm.service, hwDefs.irTherm.data, irThermCallback, function(error){console.log(error);});
+
+/**
+ * Enables the raw movement data to be sent over bluetooth. The device must also be in the correct 
+ * operating mode in order to recieve non-zero data.  Note: calls to configuration functions should 
+ * be spaced by a few miliseconds by calling them with setTimeout.
+ * @param {string} handle - MAC address string of the device you're enabling the callback for.
+ */
+exports.enableThermCallback = function(handle){
+        function findHandle(callbacks) {
+             return callbacks.handle === handle;
+        }
+
+		 ble.startNotification(handle, hwDefs.irTherm.service, hwDefs.irTherm.data, irThermCallbacks.find(findHandle).callback, function(error){console.log(error);});
+
+         var configData = new Uint8Array(1);
+		 configData[0] = 1;
+		 ble.write(handle, hwDefs.irTherm.service, hwDefs.irTherm.configuration, configData.buffer,
+		     function() { console.log("Enabled irTherm."); },function(error){console.log(error);});
 	 };
-	 exports.disableThermCallback = function(){
-		 ble.stopNotification(deviceId, hwDefs.irTherm.service, hwDefs.irTherm.data, function(){console.log("irThermometer Notifications Stopped");}, function(error){console.log(error);});
+
+/**
+ * Disables the raw movement data being sent over bluetooth.
+ * @param {string} handle - MAC address string of the device you're disabling the callback for.
+ */
+	 exports.disableThermCallback = function(handle){
+		 ble.stopNotification(handle, hwDefs.irTherm.service, hwDefs.irTherm.data, function(){console.log("irThermometer Notifications Stopped");}, function(error){console.log(error);});
+
+         var configData = new Uint8Array(1);
+		 configData[0] = 0;
+		 ble.write(handle, hwDefs.irTherm.service, hwDefs.irTherm.configuration, configData.buffer,
+		     function() { console.log("Disabled irTherm."); },function(error){console.log(error);});
 	 };
-	 exports.setThermPeriod = function(period){
+
+/**
+ * Sets the movement data period of the AHRS device. 
+ * @param {string} handle - MAC address string of the device you're changing the movement data period of.
+ * @param {value} mode - Integer value between 10 and 255.  Resolution is 10ms with a minimum of 100ms period.  For example, passing 10 yields 10 * 10ms = 100ms period.
+ */
+	 exports.setThermPeriod = function(handle, period){
 		 //TODO: Add math to calculate period value
 		 var periodData = new Uint8Array(1);
 		 periodData[0] = period;
-		 ble.write(deviceId, hwDefs.irTherm.service, hwDefs.irTherm.period, periodData.buffer,
+		 ble.write(handle, hwDefs.irTherm.service, hwDefs.irTherm.period, periodData.buffer,
 		     function() { console.log("Configured irTherm period."); },function(error){console.log(error);});
-	 };
-	 exports.enableTherm = function(){
-		 var configData = new Uint8Array(1);
-		 configData[0] = 1;
-		 ble.write(deviceId, hwDefs.irTherm.service, hwDefs.irTherm.configuration, configData.buffer,
-		     function() { console.log("Enabled irTherm."); },function(error){console.log(error);});
-	 };
-	 exports.disableTherm = function(){
-		 var configData = new Uint8Array(1);
-		 configData[0] = 0;
-		 ble.write(deviceId, hwDefs.irTherm.service, hwDefs.irTherm.configuration, configData.buffer,
-		     function() { console.log("Disabled irTherm."); },function(error){console.log(error);});
 	 };
 	 
 	 
 	 //************************************************************************
 	 //IO Service Functions
 	 //************************************************************************
-	 exports.enableLEDControl = function(){
-	     var ioConfig = new Uint8Array(1);
-	     ioConfig[0] = 1; //Enable LED Remote Control
-	     ble.write(deviceId, hwDefs.io.service, hwDefs.io.configuration, ioConfig.buffer, 
-	     	function() { console.log("Enabled LED Control."); }, app.onError);
-	 };
-	 exports.disableLEDControl = function(){
-         exports.setLEDColor(handle, 0, 0, 0);
-	     var ioConfig = new Uint8Array(1);
-	     ioConfig[0] = 0; //Disable LED Remote Control
-	     ble.write(deviceId, hwDefs.io.service, hwDefs.io.configuration, ioConfig.buffer, 
-	     	function() { console.log("Disabled LED Control."); }, app.onError);
-	 };
-	 exports.setLEDColor = function(red, green, blue){
-	        var ioValue = new Uint8Array(3);
-	        ioValue[0] = red; //Set red value
-	        ioValue[1] = green; //Set red value
-	        ioValue[2] = blue; //Set red value
-	        ble.write(deviceId, hwDefs.io.service, hwDefs.io.data, ioValue.buffer, 
-	        		function(){console.log("Sent LED Color.");}, app.onError);
-	 };
+/**
+ * Enables app control of the LED onboard the Polymorphic.Move
+ * @param {string} handle - MAC address string of the device you're changing the movement data period of.
+ */
+exports.enableLEDControl = function(handle) {
+    var ioConfig = new Uint8Array(1);
+    ioConfig[0] = 1; //Enable LED Remote Control
+    ble.write(handle, hwDefs.io.service, hwDefs.io.configuration, ioConfig.buffer,
+        function() {
+            console.log("Enabled LED Control.");
+        }, app.onError);
+};
+
+/**
+ * Disables app control of the LED onboard the Polymorphic.Move
+ * @param {string} handle - MAC address string of the device you're changing the movement data period of.
+ */
+exports.disableLEDControl = function(handle) {
+    exports.setLEDColor(handle, 0, 0, 0);
+    var ioConfig = new Uint8Array(1);
+    ioConfig[0] = 0; //Disable LED Remote Control
+    ble.write(handle, hwDefs.io.service, hwDefs.io.configuration, ioConfig.buffer,
+        function() {
+            console.log("Disabled LED Control.");
+        }, app.onError);
+};
+
+/**
+ * Sets the color of teh LED onboard the Polymorphic.Move
+ * @param {string} handle - MAC address string of the device you're changing the movement data period of.
+ * @param {value} red - 1 or 0
+ * @param {value} green - 1 or 0;
+ * @param {value} blue - 1 or 0;
+ */
+exports.setLEDColor = function(handle, red, green, blue) {
+    var ioValue = new Uint8Array(1);
+    ioValue[0] = red | (green << 1) | (blue << 2);
+    ble.write(handle, hwDefs.io.service, hwDefs.io.data, ioValue.buffer,
+        function() {
+            console.log("Sent LED Color.");
+        }, app.onError);
+};
 	 
 	 //************************************************************************
 	 //Button Service Functions
 	 //************************************************************************
-	 var buttonCallback;
-	 exports.registerButtonCallback = function(callback){
-		 buttonCallback = callback;
-	 };
-	 exports.enableButtonCallback = function(){
-		 ble.startNotification(deviceId, hwDefs.button.service, hwDefs.button.data, buttonCallback, function(error){console.log(error);});
-	 };
-	 exports.disableButtonCallback = function(){
-		 ble.stopNotification(deviceId, hwDefs.button.service, hwDefs.button.data, function(){console.log("Button Notifications Stopped");}, function(error){console.log(error);});
-	 };
+var buttonCallbacks = [];
+
+/**
+ * Registers a callback for when button data is recieved.
+ * @param {string} handle - MAC address string of the device you're registering the callback for.
+ * @param {function} callback - Callback function to recieve data.
+ */
+exports.registerButtonCallback = function(handle, callback) {
+    buttonCallbacks.push({
+        handle,
+        callback
+    });
+};
+
+/**
+ * Enables the button data to be sent over bluetooth. The device must also be in the correct operating mode in order to recieve non-zero data.  
+ * @param {string} handle - MAC address string of the device you're enabling the callback for.
+ */
+exports.enableButtonCallback = function(handle) {
+    function findHandle(callbacks) {
+        return callbacks.handle === handle;
+    }
+
+    //Start notifications
+    ble.startNotification(handle, hwDefs.button.service, hwDefs.button.data, buttonCallbacks.find(findHandle).callback, function(error) {
+        console.log(error);
+    });
+};
+
+/**
+ * Disables the button data being sent over bluetooth.
+ * @param {string} handle - MAC address string of the device you're disabling the callback for.
+ */
+exports.disableButtonCallback = function(handle) {
+    ble.stopNotification(handle, hwDefs.button.service, hwDefs.button.data, function() {
+        console.log("Button Notifications Stopped");
+    }, function(error) {
+        console.log(error);
+    });
+};
 
 	
 
