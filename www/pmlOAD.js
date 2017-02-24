75ada70a781a7ce605d4a2857a8e9f71840793d1
Trey German <treygerman@ti.com>
2017-02-24 16:31:40 -0600

    OAD Updates

--- www/pmlOAD.js
+++ www/pmlOAD.js
@@ -62,53 +62,110 @@ exports.registerOADCallback = function(handle, callback){
 
 
 exports.startOAD = function(handle, fileEntry){
-    var loadLoop = function(hexData){
-        console.log("File Loaded");
 
-        var lines = hexData.split(/[\r\n]+/g); // tolerate both Windows and Unix linebreaks
-        for(var i = 0; i < lines.length; i++) { /* do something with lines[i] */ 
-            var pos = 0;
 
-            if(lines[i].charAt(pos++) != ":"){
-			    console.log("Hex file invalid");
-                break;
-            }
+    //Contains hex file data after file is loaded
+    var hexLines;
 
-		    //Number of bytes (hex digit pairs) in the data field
-		    var dataLength = parseInt(lines[i].substr(pos, 2), 16);
-		    pos += 2;
-		    //Get 16-bit address (big-endian)
-		    var lowAddress = parseInt(lines[i].substr(pos, 4), 16);
-		    pos += 4;
-		    //Record type
-		    var recordType = parseInt(lines[i].substr(pos, 2), 16);
-		    pos += 2;
-		    //Data field (hex-encoded string)
-		    var dataField = lines[i].substr(pos, dataLength * 2);
-			    //dataFieldBuf = new Buffer(dataField, "hex");
-            //Data field buf (int array)
-            var dataFieldBuf = [];
-            for(var j = 0; j < dataLength; j++){
-                dataFieldBuf.push(parseInt(dataField.substr(j*2, 2), 16));
-            }    
+    //Total number of lines in hex file
+    var hexLength;
 
-		    pos += dataLength * 2;
-		    //Checksum
-		    var checksum = parseInt(lines[i].substr(pos, 2), 16);
-		    pos += 2;
-		    //Validate checksum
-		    var calcChecksum = (dataLength + (lowAddress >> 8) +
-			    lowAddress + recordType) & 0xFF;
-		    for(var i = 0; i < dataLength; i++)
-			    calcChecksum = (calcChecksum + dataFieldBuf[i]) & 0xFF;
-		    calcChecksum = (0x100 - calcChecksum) & 0xFF;
-		    if(checksum != calcChecksum){
-			    console.log("Invalid checksum on line " + lineNum +
-				    ": got " + checksum + ", but expected " + calcChecksum);
-                break;
-            }
-        
+    //Current hex line in memory
+    var currentLine = 0;
+
+    //Contains data from the last hex line that was parsed
+    var dataLength;
+    var lowAddress;
+    var recordType;
+    var dataField;
+    var dataFieldBuf = [];
+    var checksum;
+    var calcChecksum;
+
+    var parseHexLine = function(hexLine){
+        var pos = 0;
+
+        if(hexLine.charAt(pos++) != ":"){
+    	    console.log("Hex file invalid");
+            return -1;
+            break;
         }
+
+		//Number of bytes (hex digit pairs) in the data field
+		dataLength = parseInt(hexLine.substr(pos, 2), 16);
+		pos += 2;
+		//Get 16-bit address (big-endian)
+		lowAddress = parseInt(hexLine.substr(pos, 4), 16);
+		pos += 4;
+		//Record type
+		recordType = parseInt(hexLine.substr(pos, 2), 16);
+		pos += 2;
+		//Data field (hex-encoded string)
+		dataField = hexLine.substr(pos, dataLength * 2);
+        //Data field buf (int array)
+        dataFieldBuf = [];
+        for(var j = 0; j < dataLength; j++){
+            dataFieldBuf.push(parseInt(dataField.substr(j*2, 2), 16));
+        }    
+
+		pos += dataLength * 2;
+		//Checksum
+		checksum = parseInt(hexLine.substr(pos, 2), 16);
+		pos += 2;
+		//Validate checksum
+		calcChecksum = (dataLength + (lowAddress >> 8) +
+		    lowAddress + recordType) & 0xFF;
+		for(var i = 0; i < dataLength; i++)
+		    calcChecksum = (calcChecksum + dataFieldBuf[i]) & 0xFF;
+		calcChecksum = (0x100 - calcChecksum) & 0xFF;
+		if(checksum != calcChecksum){
+		    console.log("Invalid checksum" +
+			    ": got " + checksum + ", but expected " + calcChecksum);
+            return -1;
+            break;
+        }
+        return 0;
+
+    }
+
+    var onIdentifyNotification = function(data){
+        console.log("Metadata rejected:" + data);
+
+    }
+
+    var onBlockNotification = function(data){
+        console.log("Metadata accepted:" + data);
+
+    }
+
+
+    var onFileLoaded = function(fileData){
+        //hexData = fileData;
+
+        console.log("File Loaded");
+        
+        //Split up the hex file into lines
+        var hexLines = fileData.split(/[\r\n]+/g); // tolerate both Windows and Unix linebreaks
+        hexLength = hexLines.length;
+
+        //Config Image Identify Notifications
+        ble.startNotification(handle, hwDefs.oad.service, hwDefs.oad.imageIdentify, onIdentifyNotification, function(error) {
+            console.log(error);
+        });
+    
+        //Config Image Block Notifications
+        ble.startNotification(handle, hwDefs.oad.service, hwDefs.oad.imageBlock, onBlockNotification, function(error) {
+            console.log(error);
+        });
+
+        //Grab Metadata
+        parseHexLine(hexLines[0]);
+        var metaData = new Uint8Array(dataFieldBuf);
+
+        //Write Metadata
+        ble.writeWithoutResponse(handle, hwDefs.oad.service, hwDefs.oad.imageIdentify, metaData.buffer,
+		     function() { console.log("Sent Image Metadata."); },function(error){console.log(error);});
+
 
     };
 
@@ -149,7 +206,7 @@ exports.startOAD = function(handle, fileEntry){
         var reader = new FileReader();
 
         reader.onloadend = function (e) {
-            loadLoop(this.result);
+            onFileLoaded(this.result);
         };
 
         reader.readAsText(file);
