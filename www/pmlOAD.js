4fac585370590593ab48be98ee670cd4615bf894
Trey German <treygerman@ti.com>
2017-02-24 17:51:39 -0600

    OAD Updates

--- www/pmlOAD.js
+++ www/pmlOAD.js
@@ -134,7 +134,7 @@ exports.startOAD = function(handle, fileEntry){
     }
 
     var appendBuffer = function(buffer1, buffer2) {
-        var tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+        var tmp = new Uint8Array(buffer1.byteLength + buffer2.length);
         tmp.set(new Uint8Array(buffer1), 0);
         tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
         return tmp;
